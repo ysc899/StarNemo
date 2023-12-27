99b220d09c99a5f2097a0076b921a963857c66ba
ysc899 <59191274+ysc899@users.noreply.github.com>
2023-12-27 14:34:12 +0900

    Update LoginPO.java

--- app/src/main/java/kr/co/seesoft/nemo/starnemoapp/api/po/LoginPO.java
+++ app/src/main/java/kr/co/seesoft/nemo/starnemoapp/api/po/LoginPO.java
@@ -8,6 +8,7 @@ public class LoginPO {
     /** id sss*/
     private String UserId;
     private String UserId2;
+       private String UserId3;
     /** 패스워드 */
     private String Password;
     /** 병원코드? */
