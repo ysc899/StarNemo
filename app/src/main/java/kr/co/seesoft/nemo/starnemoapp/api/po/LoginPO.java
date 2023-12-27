fe36e1aa178a457e4005fb2b8a895273648a7d78
ysc899 <ysc899@gmail.com>
2023-12-27 14:11:16 +0900

    take2

--- app/src/main/java/kr/co/seesoft/nemo/starnemoapp/api/po/LoginPO.java
+++ app/src/main/java/kr/co/seesoft/nemo/starnemoapp/api/po/LoginPO.java
@@ -5,7 +5,7 @@ package kr.co.seesoft.nemo.starnemoapp.api.po;
  * 로그인용 PO
  */
 public class LoginPO {
-    /** id s*/
+    /** id ss*/
     private String UserId;
     /** 패스워드 */
     private String Password;
